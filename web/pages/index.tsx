258d70ec3a454e4030d2fbe2d03d1fd8bdd5f918
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2022-11-03 22:14:43 -0300

    Testando conceitos do React

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,8 +1,15 @@
 //JSX - JavaScript + XML (HTML)
 //TSX - TypeScript + JSX
 
+import { Tweet } from "../components/Tweet";
+
 export default function Home() {
   return (
-    <h1>Hello NLW</h1>
+    <div>
+      <Tweet text="Meu primeiro tweet"/>
+      <Tweet text="Meu segundo tweet"/>
+      <Tweet text="Meu terceiro tweet"/>
+      <Tweet text="Meu quarto tweet"/>
+    </div>
   )
 }
