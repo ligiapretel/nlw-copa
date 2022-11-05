5fad1f0d41b35624d376438155eb150c65031bc8
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2022-11-05 19:47:55 -0300

    Criando funcionalidade para criação do bolão

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { z } from "zod";//Biblioteca para validação de dados
 import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";//Biblioteca para gerar o códgio do bolão aleatório
 
 const prisma = new PrismaClient({
     //Para visualizar as queries no terminal
@@ -20,9 +22,36 @@ async function bootstrap() {
     //Primeira rota
     fastify.get("/pools/count", async () => {
         //Fazendo a contagem de bolões
-        const count = await prisma.pool.count()
+        const count = await prisma.pool.count();
 
-        return { count }
+        return { count };
+    });
+
+    //Rota para criar um bolão
+    fastify.post("/pools", async (request, reply) => {
+        //Validando campo title para que não seja null 
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        //Recebendo título do bolão
+        const { title } = createPoolBody.parse(request.body);
+
+        //Gerando o código do bolão com 6 caracteres
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toUpperCase();
+
+        //Criando o bolão
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        });
+
+        return reply.status(201).send({ code });
+
+       ;
     });
     
     //O host é para funcionar no mobile
